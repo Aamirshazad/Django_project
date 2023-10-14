6b1c6190abb1e88d7db85ba18435f312ad04bf89
AamirShazad <amiralicomsats3gmail.com>
2023-10-14 18:38:04 +0500

    add new files

---
+++ test_python.py
@@ -0,0 +1,19 @@
+import unittest
+from my_code import my_function
+
+class TestMyFunction(unittest.TestCase):
+    
+    def test_my_function_with_valid_input(self):
+        result = my_function(5)
+        self.assertEqual(result, 25)
+        
+    def test_my_function_with_negative_input(self):
+        result = my_function(-5)
+        self.assertEqual(result, 0)
+        
+    def test_my_function_with_zero_input(self):
+        result = my_function(0)
+        self.assertEqual(result, 0)
+        
+if __name__ == '__main__':
+    unittest.main()
